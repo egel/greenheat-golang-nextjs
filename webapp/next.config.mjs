dabbdcf8c8c92bcb455efef37cae439f361ab91a
Maciej Sypien <maciejsypien@gmail.com>
2024-10-08 19:57:38 +0200

    feat: add auto redirect to dashboard

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
