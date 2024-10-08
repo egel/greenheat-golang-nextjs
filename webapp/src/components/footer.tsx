35355d5d00832921ae3d86d1014ee9726bf2a64b
Maciej Sypien <maciejsypien@gmail.com>
2024-10-08 20:08:45 +0200

    feat: improve footer

--- webapp/src/components/footer.tsx
+++ webapp/src/components/footer.tsx
@@ -1,53 +1,13 @@
-import Image from "next/image";
+import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="https://nextjs.org/icons/file.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        Learn
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="https://nextjs.org/icons/window.svg"
-          alt="Window icon"
-          width={16}
-          height={16}
-        />
-        Examples
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="https://nextjs.org/icons/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        Go to nextjs.org →
-      </a>
+    <footer className="my-7 flex flex-wrap items-center justify-center">
+      <div className="flex flex-row gap-2 items-center text-gray-400">
+        <span>Made with</span>
+        <FaHeart className="transition ease-in-out hover:animate-pulse hover:text-red-400 cursor-pointer" />
+        <span>by Maciej Sypien for demonstation purposes</span>
+      </div>
     </footer>
   );
 }
